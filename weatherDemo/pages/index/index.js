474e0f4e225c20ec131524c2ce88bb3ef3b20cc4
Coinc1dens <846899048@qq.com>
2022-08-17 15:41:41 +0800

    upd weatherDemo

--- weatherDemo/pages/index/index.js
+++ weatherDemo/pages/index/index.js
@@ -1,17 +1,47 @@
+const { getLocationID } = require("../../utils/util");
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    
+    region:['山东省','青岛市','崂山区'],
+    now:{
+      temp:0,
+      text:'未知',
+      icon:'999',
+      windSpeed:"0",
+      windScale:"0",
+      windDir:"未知",
+      vis:"0",
+      pressure:"0",
+      humidity:"0",
+    }
   },
-
+  regionChange: function(e){
+    this.setData({region: e.detail.value});
+    this.getWeather();
+  },
+  getWeather: function() {
+    var that = this;
+    var locID = getLocationID(that.data.region[1]);
+    wx.request({
+      url: 'https://devapi.qweather.com/v7/weather/now',
+      data: {
+        location:locID,
+        key:'7106852dc76b49d78c290d9460740a19'
+      },
+      success:function(res){
+        that.setData({now:res.data.now});
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    this.getWeather();
   },
 
   /**
